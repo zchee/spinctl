faf5c1fd9c97629c138be63e4b9978aa004dd512
Koichi Shiraishi <zchee.io@gmail.com>
2018-09-18 16:40:07 +0900

    pkg/version: add initial version package

---
+++ pkg/version/version.go
@@ -0,0 +1,9 @@
+// Copyright 2018 The spinctl Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package version provides the current spinctl version.
+package version
+
+// Version is the current spinctl version.
+const Version = "0.0.1-dev"
