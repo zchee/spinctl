aaf9738411d82a46e77f2c65e4fffd7b6328248d
Koichi Shiraishi <zchee.io@gmail.com>
2018-10-08 09:56:15 +0900

    pkg/logger: switch to zap.SugaredLogger by default

--- pkg/logger/context.go
+++ pkg/logger/context.go
@@ -11,15 +11,37 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-type ctxLogger struct{}
+type (
+	ctxLogger        struct{}
+	ctxSugaredLogger struct{}
+)
 
-var ctxLoggerKey = &ctxLogger{}
+var (
+	ctxLoggerKey        = &ctxLogger{}
+	ctxSugaredLoggerKey = &ctxSugaredLogger{}
+)
 
-func NewContext(ctx context.Context, logger *zap.Logger) context.Context {
+func NewContext(ctx context.Context, sugaredLogger *zap.SugaredLogger) context.Context {
+	return context.WithValue(ctx, ctxSugaredLoggerKey, sugaredLogger)
+}
+
+func NewContextLogger(ctx context.Context, logger *zap.Logger) context.Context {
 	return context.WithValue(ctx, ctxLoggerKey, logger)
 }
 
-func FromContext(ctx context.Context) *zap.Logger {
+func NewContextSugaredLogger(ctx context.Context, sugaredLogger *zap.SugaredLogger) context.Context {
+	return NewContext(ctx, sugaredLogger)
+}
+
+func FromContext(ctx context.Context) *zap.SugaredLogger {
+	l, ok := ctx.Value(ctxSugaredLoggerKey).(*zap.SugaredLogger)
+	if !ok {
+		return nil
+	}
+	return l
+}
+
+func FromContextLogger(ctx context.Context) *zap.Logger {
 	l, ok := ctx.Value(ctxLoggerKey).(*zap.Logger)
 	if !ok {
 		return nil
@@ -27,6 +49,10 @@ func FromContext(ctx context.Context) *zap.Logger {
 	return l
 }
 
+func FromContextSugaredLogger(ctx context.Context) *zap.SugaredLogger {
+	return FromContext(ctx)
+}
+
 func WithContext(ctx context.Context, fields ...zapcore.Field) context.Context {
-	return NewContext(ctx, FromContext(ctx).With(fields...))
+	return NewContextLogger(ctx, FromContextLogger(ctx).With(fields...))
 }
