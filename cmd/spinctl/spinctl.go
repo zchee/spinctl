0f6364dd5124ae22f7fa7a00e35e8c5dbec87b83
Koichi Shiraishi <zchee.io@gmail.com>
2018-10-08 09:46:30 +0900

    cmd/spinctl: use logger.NewZapSugaredLogger

--- cmd/spinctl/spinctl.go
+++ cmd/spinctl/spinctl.go
@@ -9,15 +9,24 @@ import (
 	"context"
 	"os"
 
+	"go.uber.org/zap/zapcore"
+
 	"github.com/zchee/spinctl/pkg/cmd"
+	"github.com/zchee/spinctl/pkg/logger"
 )
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	log := logger.NewZapSugaredLogger(zapcore.InfoLevel, os.Stdout)
+	ctx = logger.NewContext(ctx, log)
+
 	c := cmd.NewDefaultCommand(ctx, os.Args[1:])
 	if err := c.Execute(); err != nil {
-		os.Exit(1)
+		switch err.(type) {
+		default:
+			log.Fatal("main")
+		}
 	}
 }
