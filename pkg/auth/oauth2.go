c4f562d79744e83e2f9199244cd71c4fcb552c2d
Koichi Shiraishi <zchee.io@gmail.com>
2018-12-21 08:06:45 +0900

    pkg/auth: implements verifierHash and verifierAndCode

--- pkg/auth/oauth2.go
+++ pkg/auth/oauth2.go
@@ -7,11 +7,14 @@ package auth
 import (
 	"bufio"
 	"context"
+	crand "crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"net/http"
 	"os"
 	"strings"
 
+	sha256 "github.com/minio/sha256-simd"
 	"github.com/pkg/errors"
 	open "github.com/zchee/go-open"
 	"golang.org/x/oauth2"
@@ -69,6 +72,7 @@ func AuthenticateOAuth2(ctx context.Context, oc *OAuth2Config) (*oauth2.Token, e
 	//  https://tools.ietf.org/html/rfc7636
 	//  https://www.oauth.com/oauth2-servers/pkce/authorization-code-exchange/
 	//  https://developer.okta.com/authentication-guide/implementing-authentication/auth-code-pkce
+	//  https://developers.google.com/identity/protocols/OpenIDConnect#server-flow
 	authURL := conf.AuthCodeURL("state",
 		oauth2.AccessTypeOffline,
 		oauth2.ApprovalForce,
@@ -98,4 +102,25 @@ func codeFromStdin() string {
 	code, _ := r.ReadString('\n')
 
 	return strings.TrimSpace(code)
+}
+
+func verifierHash(b []byte) (verifierHash []byte) {
+	h := sha256.New()
+	h.Write(b)
+	verifierHash = h.Sum(nil)
+
+	return verifierHash
+}
+
+func verifierAndCode() (verifier, code string, err error) {
+	b := make([]byte, 1024)
+	if _, err := crand.Read(b); err != nil {
+		return "", "", errors.Wrap(err, "could not generate random string")
+	}
+
+	verifier = base64.RawURLEncoding.EncodeToString(b)
+	verifierHash := verifierHash([]byte(verifier))
+	code = base64.RawURLEncoding.EncodeToString(verifierHash) // TODO(zchee): optimize of byteslice to string
+
+	return verifier, code, nil
 }
