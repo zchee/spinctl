be228eee63c5b76dd07137c13eb151a599e3e2a5
Koichi Shiraishi <zchee.io@gmail.com>
2019-01-02 16:15:30 +0900

    pkg/version: fix package document

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package version provides the current spinctl version.
+// Package version provides the spinctl version.
 package version
 
 var (
