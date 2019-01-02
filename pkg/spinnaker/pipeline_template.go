97408a2ffe163851369866a95b310e2a999af7dd
Koichi Shiraishi <zchee.io@gmail.com>
2019-01-02 20:46:31 +0900

    pkg/spinnaker: implements ListPipelineTemplates

--- pkg/spinnaker/pipeline_template.go
+++ pkg/spinnaker/pipeline_template.go
@@ -8,7 +8,9 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/antihax/optional"
 	"github.com/pkg/errors"
+	"github.com/zchee/spinctl/api/gate"
 )
 
 // GetPipelineTemplate gets a pipeline template.
@@ -24,6 +26,34 @@ func (c *Client) GetPipelineTemplate(ctx context.Context, id, format string) (st
 		// nothing to do
 	default:
 		return "", errors.Wrapf(err, "encountered an error getting pipeline template, id: %s, status code: %d\n", resp.StatusCode)
+	}
+
+	s, err := parsePayload(&payload, format)
+	if err != nil {
+		return "", err
+	}
+
+	return s, nil
+}
+
+// ListPipelineTemplates lists pipeline templates.
+func (c *Client) ListPipelineTemplates(ctx context.Context, scopes []string, format string) (string, error) {
+	var opts gate.ListUsingGETOpts
+	if scopes != nil {
+		opts.Scopes = optional.NewInterface(scopes)
+	}
+
+	payload, resp, err := c.Client.PipelineTemplatesControllerApi.ListUsingGET(ctx, &opts)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to get list of pipeline templates")
+	}
+	defer resp.Body.Close()
+
+	switch resp.StatusCode {
+	case http.StatusOK:
+		// nothing to do
+	default:
+		return "", errors.Wrapf(err, "encountered an error getting list of pipeline templates, scopes %s, status code: %d\n", scopes, resp.StatusCode)
 	}
 
 	s, err := parsePayload(&payload, format)
