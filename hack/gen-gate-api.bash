b40eac2e7362c1ff9dfdedeb9caaac4449cf2930
Koichi Shiraishi <zchee.io@gmail.com>
2019-01-15 04:03:51 +0900

    hack/gen-gate-api.bash: fix goimports command order

--- hack/gen-gate-api.bash
+++ hack/gen-gate-api.bash
@@ -16,6 +16,7 @@ PKG_ROOT="$GO_PATH/src/github.com/zchee/spinctl"
 
 rm -rf "$PKG_ROOT/api/gate"
 swagger-codegen generate --auth "cookie: SESSION=$SESSION" -i "$ENDPOINT/v2/api-docs" -l go -D 'packageName=gate' -D "packageVersion=$SPINNAKER_VERSION" -o "$PKG_ROOT/api/gate"
+goimports -w api/gate
 
 curl -sv -H "cookie: SESSION=$SESSION" -o "$PKG_ROOT/api/swagger-spec/gate-$SPINNAKER_VERSION-raw.json" "$ENDPOINT/v2/api-docs"
 if [[ -f "$PKG_ROOT/api/swagger-spec/gate-$SPINNAKER_VERSION.json" ]]; then
@@ -23,8 +24,6 @@ if [[ -f "$PKG_ROOT/api/swagger-spec/gate-$SPINNAKER_VERSION.json" ]]; then
 fi
 jq ' ' < "$PKG_ROOT/api/swagger-spec/gate-$SPINNAKER_VERSION-raw.json" > "$PKG_ROOT/api/swagger-spec/gate-$SPINNAKER_VERSION.json"
 rm -f "$PKG_ROOT/api/swagger-spec/gate-$SPINNAKER_VERSION-raw.json"
-
-goimports -w api/gate
 
 git add api/gate
 git add api/swagger-spec/"gate-$SPINNAKER_VERSION.json"
