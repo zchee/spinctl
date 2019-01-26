d5fc2c58b08bd24bc61eba9fe23fd5f7c9348c72
Koichi Shiraishi <zchee.io@gmail.com>
2019-01-26 20:39:30 +0900

    api/gate: regenerate gate API from spinnaker master-latest-validated-20190124010133

--- api/gate/configuration.go
+++ api/gate/configuration.go
@@ -62,7 +62,7 @@ func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		BasePath:      "https://localhost:8084",
 		DefaultHeader: make(map[string]string),
-		UserAgent:     "Swagger-Codegen/master-latest-validated-20190121213415/go",
+		UserAgent:     "Swagger-Codegen/master-latest-validated-20190124010133/go",
 	}
 	return cfg
 }
