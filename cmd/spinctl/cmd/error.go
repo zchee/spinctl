98092da0dec1623d550a2bf2d03b3c831c1a2598
Koichi Shiraishi <zchee.io@gmail.com>
2019-01-27 18:06:09 +0900

    cmd/spinctl/cmd: add errNotImplementedYet variable

---
+++ cmd/spinctl/cmd/error.go
@@ -0,0 +1,13 @@
+// Copyright 2019 The spinctl Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"errors"
+)
+
+var (
+	errNotImplementedYet = errors.New("not implemented yet.")
+)
