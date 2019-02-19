24863ec737151a9e165c7759aeebe393039909f5
Koichi Shiraishi <zchee.io@gmail.com>
2019-02-20 05:22:01 +0900

    pkg/logging: fix package comment

--- pkg/logging/doc.go
+++ pkg/logging/doc.go
@@ -2,5 +2,5 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package logging provives the zap.Logger into the context.Context.
+// Package logging provives the zap.SugaredLogger.
 package logging
