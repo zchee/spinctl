8abedddd6d2048f22a1eef365b5965f9f9c30785
Koichi Shiraishi <zchee.io@gmail.com>
2019-02-21 03:20:46 +0900

    hack/gen-gate-api: trim timestamp if 'master-latest-validated' version

--- hack/gen-gate-api.bash
+++ hack/gen-gate-api.bash
@@ -10,12 +10,17 @@ IFS=$'\n\t'
 
 ENDPOINT="$1"
 SPINNAKER_VERSION="$2"
+SPINNAKER_VERSION_FULL="$2"
 SESSION="$3"
 GO_PATH=${GOPATH:-$HOME/go}
 PKG_ROOT="$GO_PATH/src/github.com/zchee/spinctl"
 
+if [[ "$SPINNAKER_VERSION" == *"master-latest-validated-"* ]]; then
+  SPINNAKER_VERSION='master-latest-validated'
+fi
+
 rm -rf "$PKG_ROOT/api/gate"
-swagger-codegen generate --auth "cookie: SESSION=$SESSION" -i "$ENDPOINT/v2/api-docs" -l go -D 'packageName=gate' -D "packageVersion=$SPINNAKER_VERSION" -o "$PKG_ROOT/api/gate"
+swagger-codegen generate --auth "cookie: SESSION=$SESSION" -i "$ENDPOINT/v2/api-docs" -l go -D 'packageName=gate' -D "packageVersion=$SPINNAKER_VERSION_FULL" -o "$PKG_ROOT/api/gate"
 goimports -w api/gate
 
 curl -sv -H "cookie: SESSION=$SESSION" -o "$PKG_ROOT/api/swagger-spec/gate-$SPINNAKER_VERSION-raw.json" "$ENDPOINT/v2/api-docs"
